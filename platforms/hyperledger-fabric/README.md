4b8823cf9eed01da411167c00a7de71b29362065
renjithpta <renjithpta@gmail.com>
2021-12-11 07:23:44 +0000

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: renjithpta <renjithpta@gmail.com>

--- platforms/hyperledger-fabric/README.md
+++ platforms/hyperledger-fabric/README.md
@@ -4,7 +4,7 @@
 [//]: # (##############################################################################################)
 
 ## About
-This folder contains the files that are needed for the deployment of a Hyperledger Fabric network in Hyperledger Bevel. 
+This folder contains the files that are needed for the deployment of a Hyperledger Fabric network in BAF. 
 
 ## Folder structure
 ```
--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/bevel/build/config
+KUBECONFIG=/home/ubuntu/bevel/build/config
 
 echo "Validatin network yaml"
-ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
+ajv validate -s /home/ubuntu/bevel/platforms/network-schema.json -d /home/ubuntu/bevel/build/network.yaml 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/ubuntu/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/bevel/platforms/shared/inventory/ -e "@/home/ubuntu/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
